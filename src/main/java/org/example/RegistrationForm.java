ce3c6dd2d8be6ea4e5c900df109b28693f8ddab6
baraabd <bara.abdullatif@outlook.com>
2024-11-06 19:53:32 +0100

    Fix username and password validation back to original requirements

--- src/main/java/org/example/RegistrationForm.java
+++ src/main/java/org/example/RegistrationForm.java
@@ -21,7 +21,7 @@ public class RegistrationForm {
     public void setPassword(String password) { this.password = password; }
 
     public boolean isValidUsername() {
-        return username != null && username.length() >= 2;
+        return username != null && username.length() >= 5;
     }
 
     public boolean isValidEmail() {
@@ -29,7 +29,7 @@ public class RegistrationForm {
     }
 
     public boolean isValidPassword() {
-        return password != null && password.length() >= 3;
+        return password != null && password.length() >= 8;
     }
 
 }
