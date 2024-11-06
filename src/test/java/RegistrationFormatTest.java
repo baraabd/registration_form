72886d07fda2937aad3e5d3d1313324383ca0641
baraabd <bara.abdullatif@outlook.com>
2024-11-06 14:44:52 +0100

    Import assertFalse module

--- src/test/java/RegistrationFormatTest.java
+++ src/test/java/RegistrationFormatTest.java
@@ -1,6 +1,7 @@
 import org.example.RegistrationForm;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class RegistrationFormatTest {
